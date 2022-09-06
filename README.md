1eb80947cc6143a41e7890a27a16ad1640934bbe
YellowFlash2012 <cyclop9@hotmail.com>
2022-09-06 06:21:18 +0100

    primer on django about url patterns, views, models and data flow

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # sarda-drf
 DRF Masterclass
+
+https://github.com/ShubhamSarda/MovieListAPI-Django
+
+https://www.django-rest-framework.org/
+
+https://github.com/ShubhamSarda/IMDB-Clone-DRF
--- watchlist_app/views.py
+++ watchlist_app/views.py
@@ -1,3 +1,27 @@
+from django.http import JsonResponse
 from django.shortcuts import render
 
+from watchlist_app.models import Movie
+
 # Create your views here.
+def get_all_movies(request):
+    # get query_sets
+    movies = Movie.objects.all()
+
+    # step2: convert query_set into python dictionqry
+    data={
+        'movies': list(movies.values())
+    }
+
+    # step3: convert python dictionqry into json
+    return JsonResponse(data)
+
+def get_single_movie(request, pk):
+    movie = Movie.objects.get(pk=pk)
+
+    data = {
+        "name":movie.name,
+        "launched":movie.launched
+    }
+
+    return JsonResponse(data)
